8cfcbe97dc9ad44465871fe2598232c512b8735c
JustAlexander <aleksep@gmail.com>
2022-05-03 14:08:31 +0000

    first

--- remix/app/components/payments/types.ts
+++
@@ -1,7 +0,0 @@
-import { CheckoutModelInput } from "../../service-api/types.generated";
-
-export interface PaymentProps {
-  checkoutModel: CheckoutModelInput;
-  onSuccess?: ({ orderId: string }) => void;
-  onError?: (error: Error) => void;
-}
--- remix/app/config/locale.ts
+++
@@ -1,7 +0,0 @@
-export const locale = {
-  locale: "en",
-  displayName: "English - US",
-  appLanguage: "en-US",
-  crystallizeCatalogueLanguage: "en",
-  crystallizePriceVariant: "default",
-};
